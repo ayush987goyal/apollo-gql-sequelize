856a5f95f7ca60bfe41a282c698bd4124cbeb1af
Ayush Goyal <ayush987goyal@gmail.com>
2018-08-30 01:13:00 +0530

    Use entities in resolvers

--- src/index.js
+++ src/index.js
@@ -21,15 +21,55 @@ const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
   playground,
-  context: {
+  context: async () => ({
     models,
-  }
+    me: await models.User.findByLogin('rwieruch')
+  })
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-sequelize.sync().then(async () => {
+const eraseDatabaseOnSync = true;
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
   app.listen({ port: 8080 }, () => {
     console.log('Server running on http://localhost:8080/graphql');
   });
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      messages: [
+        {
+          text: 'Published the Road to learn React'
+        }
+      ]
+    },
+    {
+      include: [models.Message]
+    }
+  );
+
+  await models.User.create(
+    {
+      username: 'ddavids',
+      messages: [
+        {
+          text: 'Happy to release ...'
+        },
+        {
+          text: 'Published a complete ...'
+        }
+      ]
+    },
+    {
+      include: [models.Message]
+    }
+  );
+};
