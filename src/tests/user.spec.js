baeaf34238bb5865db5839b570ff197fb39ebce6
Ayush Goyal <ayush987goyal@gmail.com>
2018-09-02 19:39:10 +0530

    Add basic user test

--- src/tests/user.spec.js
+++ src/tests/user.spec.js
@@ -1,7 +1,36 @@
 import { expect } from 'chai';
 
-describe('user', () => {
-  it('user is user', () => {
-    expect('user').to.eql('user');
+import * as userApi from './api';
+
+describe('users', () => {
+  describe('user(id: String!): User', () => {
+    it('returns a user when user can be found', async () => {
+      const expectedResult = {
+        data: {
+          user: {
+            id: '1',
+            username: 'rwieruch',
+            email: 'hello@robin.com',
+            role: 'ADMIN'
+          }
+        }
+      };
+
+      const result = await userApi.user({ id: '1' });
+
+      expect(result.data).to.eql(expectedResult);
+    });
+
+    it('returns null when user cannot be found', async () => {
+      const expectedResult = {
+        data: {
+          user: null
+        }
+      };
+
+      const result = await userApi.user({ id: '42' });
+
+      expect(result.data).to.eql(expectedResult);
+    });
   });
 });
